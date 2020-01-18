e0cb179f96f7cebfb4579fac8767352a6bda8489
Lukas Streit <streit@cs.tum.edu>
2020-01-18 15:53:19 +0100

    improve pipeline and model

--- models/lstm_classifier.py
+++ models/lstm_classifier.py
@@ -7,15 +7,15 @@ from tensorflow.keras.layers import Dense, LSTM
 # Model inputs: sequence of n x m word embeddings
 
 
-def create_model(embedding_dim=128):
+def create_model(lstm_layer_size=32, embedding_dim=128, output_dim=100):
     """
     creates and returns a simple LSTM model
     :param embedding_dim: size of the input embeddings
     :return: model
     """
     model = Sequential()
-    model.add(LSTM(32, input_shape=(None, embedding_dim)))
-    model.add(Dense(32, activation="sigmoid"))
+    model.add(LSTM(lstm_layer_size, input_shape=(None, embedding_dim)))
+    model.add(Dense(output_dim, activation="sigmoid"))
     model.compile(loss="mean_squared_error", optimizer="adam")
     return model
 
