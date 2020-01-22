479781c8383d848a6285ed1b4b90dbd6a0c103c1
danielschroter <daniel@schroter.biz>
2020-01-22 16:35:55 +0100

    Pipeline Evaluation Section and output_evaluation function

--- toolbox/evaluation.py
+++ toolbox/evaluation.py
@@ -1,6 +1,5 @@
 from sklearn.metrics import f1_score, hamming_loss, recall_score, precision_score
 
-
 def binarize_model_output(y, threshold=0.5):
     return y > threshold
 
@@ -16,3 +15,10 @@ def evaluate_multiLabel(l_true, l_pred):
     print(f" Macro Evaluation: f1_Score= {f1_macro} , Recall = {recall_macro} , Precision = {precision_macro}")
     print(f" Micro Evaluation: f1_Score= {f1_micro} , Recall = {recall_micro} , Precision = {precision_micro}")
 
+
+def output_evaluation(model, sample_size, max_question_words, n_top_labels, l_true, l_pred):
+    print(f"Model Evaluation\n")
+    print(f"Parameter Settings:\n Sample size = {sample_size}, Max. number of words per question = {max_question_words}, Number of Top Labels used = {n_top_labels}\n")
+    print(model.summary())
+    print("\nMetrics:")
+    evaluate_multiLabel(l_true, l_pred)
