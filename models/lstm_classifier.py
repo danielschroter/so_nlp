6e89a6e6283909a85603530e6e3969e26950f86e
Lukas Streit <streit@cs.tum.edu>
2020-01-27 19:37:23 +0100

    add masking layer to lstm classifier

--- models/lstm_classifier.py
+++ models/lstm_classifier.py
@@ -2,24 +2,26 @@ import pandas as pd
 
 # example of a model defined with the sequential api
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.layers import Dense, LSTM, Masking
 from losses.weighted_cross_entropy import WeightedBinaryCrossEntropy
 
 # Model inputs: sequence of n x m word embeddings
 
 
-def create_model(lstm_layer_size=256, embedding_dim=128, output_dim=100):
+def create_model(lstm_layer_size=256, embedding_dim=128, output_dim=100, mask_value=None):
     """
     creates and returns a simple LSTM model
     :param embedding_dim: size of the input embeddings
     :return: model
     """
     model = Sequential()
+    model.add(Masking(mask_value=mask_value, input_shape=(None, embedding_dim)))
     model.add(LSTM(lstm_layer_size, input_shape=(None, embedding_dim)))
+    
     model.add(Dense(256, activation="relu"))
     model.add(Dense(128, activation="relu"))
     model.add(Dense(output_dim, activation="sigmoid"))
-    model.compile(loss=WeightedBinaryCrossEntropy(2, 1), optimizer="adam", metrics=["accuracy"])
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
     return model
 
 
